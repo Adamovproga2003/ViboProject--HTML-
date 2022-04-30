f08df20c8635b85570d0c26ce728e2c2ee35b5b3
Adamovproga2003 <62895330+Adamovproga2003@users.noreply.github.com>
2022-04-30 23:50:39 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// const firstParagraph = document.querySelector('.first-description');
+// const descriptionInfoContainerFirst = document.querySelector('.description-info-container-1');
+//
+// descriptionInfoContainerFirst.addEventListener("mouseover", () => {
+//   console.log('In func');
+//   firstParagraph.style.display = 'block';
+// });
+// descriptionInfoContainerFirst.addEventListener("mouseout", () => {
+//   console.log('In func');
+//   firstParagraph.style.display = 'none';
+// });
